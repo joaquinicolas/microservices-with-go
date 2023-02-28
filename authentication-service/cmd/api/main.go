de0be5e52013b9392e7b548fbf254fbc73561b7b
Joaquin Noe <joako.n96@gmail.com>
2023-02-28 15:39:34 +0000

    WIP: Add services

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"log"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,5 @@
+module authentication
+
+go 1.20
+
+require golang.org/x/crypto v0.6.0
